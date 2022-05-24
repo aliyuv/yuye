fcc47c0fbb54d2fbc533d2b9505d676f9341ab95
aliyu <1324149139@qq.com>
2022-05-24 16:02:38 +0800

    完成表单支持 v-model

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,19 @@
     <form action="">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input
-          type="text" class="form-control" id="exampleInputEmail1"
-          v-model="emailRef.val"
-          @blur="validdataEmail"
-        >
-        <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <validate-input :rules="emailRules" v-model="emailVal"></validate-input>
+        {{emailVal}}
       </div>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import columnList, { ColumnProps } from '@/components/ColumnList.vue'
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
 
 const currentUser: UserProps = {
   isLogin: true,
@@ -56,33 +49,30 @@ const testData: ColumnProps[] = [
     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
 ]
-const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export default defineComponent({
   name: 'App',
   components: {
     // columnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
-    const validdataEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = 'can not be empty '
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = 'should be valid email'
+    const emailVal = ref('aliyu')
+    const emailRules: RulesProp = [
+      {
+        type: 'required',
+        message: '电子邮箱地址不能为空'
+      },
+      {
+        type: 'email',
+        message: '请输入正确的电子邮箱格式'
       }
-    }
+    ]
     return {
       list: testData,
       currentUser,
-      emailRef,
-      validdataEmail
+      emailRules,
+      emailVal
     }
   }
 })
