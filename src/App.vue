a8b740b6d848387514cf21469ee3c92056f0714f
aliyu <1324149139@qq.com>
2022-05-25 14:11:51 +0800

    添加路由

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,39 @@
 <template>
   <div class="container">
     <GlobalHeader :user="currentUser"></GlobalHeader>
-    <!--    <columnList :list="list"></columnList>-->
-    <validate-form @form-submit="onFromSubmit">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <validate-input :rules="emailRules" v-model="emailVal" ref="inputRef"></validate-input>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">密码</label>
-        <validate-input v-model="passwordVal" :rules="passwordRules"></validate-input>
-      </div>
-      <template #submit>
-        <span class="btn btn-danger">提交</span>
-      </template>
-    </validate-form>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2022 余也专栏</li>
+          <li class="list-inline-item">我❤</li>
+          <li class="list-inline-item">厄</li>
+          <li class="list-inline-item">加</li>
+          <li class="list-inline-item">特</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
+import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { testData } from '@/testData'
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
-import ValidateForm from '@/components/ValidateForm.vue'
 
 const currentUser: UserProps = {
-  isLogin: true,
+  isLogin: false,
   name: 'aliyu'
 }
 
 export default defineComponent({
   name: 'App',
   components: {
-    // columnList,
-    GlobalHeader,
-    ValidateInput,
-    ValidateForm
+    GlobalHeader
   },
   setup () {
-    const inputRef = ref<any>()
-    const emailVal = ref('aliyu@test.com')
-    const emailRules: RulesProp = [
-      {
-        type: 'required',
-        message: '电子邮箱地址不能为空'
-      },
-      {
-        type: 'email',
-        message: '请输入正确的电子邮箱格式'
-      }
-    ]
-    const passwordVal = ref('123')
-    const passwordRules: RulesProp = [
-      {
-        type: 'required',
-        message: '密码不能为空'
-      }
-    ]
-    const onFromSubmit = (result: boolean) => {
-      console.log(result)
-    }
     return {
-      list: testData,
-      currentUser,
-      emailRules,
-      emailVal,
-      onFromSubmit,
-      inputRef,
-      passwordVal,
-      passwordRules
+      currentUser
     }
   }
 })
